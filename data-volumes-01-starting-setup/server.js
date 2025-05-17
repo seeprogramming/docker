2365dcf60cc2249acf6ed803f53aea638e67a02c
seeprogramming <sagarshelke18@gmail.com>
2025-05-17 23:34:26 +0530

    updated codebase for new docker app

--- data-volumes-01-starting-setup/server.js
+++ data-volumes-01-starting-setup/server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+console.log('HELLO world');
 app.use(express.static('public'));
 app.use('/feedback', express.static('feedback'));
 
